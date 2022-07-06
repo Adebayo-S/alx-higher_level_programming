7d895c20d88a164af00ecf50ca02ba83082715b9
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-06 02:15:41 +0100

    [fix]: used exception for file not found

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = load_from_json_file('add_item.json')
-if my_list is None:
+try:
+    my_list = load_from_json_file('add_item.json')
+except FileNotFoundError:
     my_list = []
 
 for argv in sys.argv[1:]:
