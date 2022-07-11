9a77babf15229c5c41c5d25df98bcfd7f6509fa7
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-11 21:49:04 +0100

    [fix]: added validation to setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,6 +22,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """setter width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -32,6 +37,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -42,6 +51,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -49,7 +62,11 @@ class Rectangle(Base):
         """getter y"""
         return self.__x
 
-    @x.setter
+    @y.setter
     def y(self, value):
         """setter y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
