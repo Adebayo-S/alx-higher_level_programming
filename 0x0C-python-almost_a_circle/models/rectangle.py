f8f6ab870d9d883042f961abe02d3587b9bb6c53
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-11 21:26:01 +0100

    [feat]: rectangle class added

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+class Rectangle: is the base for all other classes in this project
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize the Rectangle object"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__x
+
+    @x.setter
+    def y(self, value):
+        """setter y"""
+        self.__y = value
