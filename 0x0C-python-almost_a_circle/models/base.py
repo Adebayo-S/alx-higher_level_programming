42de61333db1fa53746342cb090f889084cfca35
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-11 21:25:40 +0100

    [feat]: base class added

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+class Base: is the base for all other classes in this project
+"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize id, increment class attribute if no id
+        and set as id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
