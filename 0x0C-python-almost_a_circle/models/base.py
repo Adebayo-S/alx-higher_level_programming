76f11412132417f9c52372cee127d7797b7f65df
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-12 13:45:44 +0100

    [feat] save_to_file and from_json_string methods added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,18 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        list_dict = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dict.append(cls.to_dictionary(obj))
+        with open("{:s}.json".format(cls.__name__), mode='w') as file:
+            file.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
