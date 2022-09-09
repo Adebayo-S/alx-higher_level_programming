e9a12f4fd2323b255a101390bf9ef8f65b07cffb
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-09 01:41:29 +0100

    [fix]: removed curly braces

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,9 +2,7 @@
 
 const list = require('./100-data').list;
 
-const newList = list.map((num, index) => {
-  num * index;
-});
+const newList = list.map((num, index) => num * index);
 
 console.log(list);
 console.log(newList);
