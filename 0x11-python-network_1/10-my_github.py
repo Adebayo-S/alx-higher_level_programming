629d0076f8ad641a7c52d4d3145c54a112a5cc02
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-24 00:38:11 +0100

    [fix]: the api has a user endpoint

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/'
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
     token = sys.argv[2]
     auth = HTTPBasicAuth(username, token)
