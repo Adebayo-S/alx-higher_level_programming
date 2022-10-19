2b7b017a525468d706b8c42afe0f8b786d3f0078
Adebayo Samuel <eladebayoor@gmail.com>
2022-10-19 02:30:07 +0100

    [style]: semistandard requirement

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let url = process.argv[2]
-const request = require('request')
+const url = process.argv[2];
+const request = require('request');
 
 request.get(url).on('response', (response) => {
-  console.log(`code: ${response.statusCode}`)
-})
+  console.log(`code: ${response.statusCode}`);
+});
